ce27adf88b9832b97961436e709d3c6c5887e19a
Deepesh <sainideepesh12@gmail.com>
2025-03-18 14:40:57 +0530

    Display-product

--- vite-project/src/component/Header.jsx
+++ vite-project/src/component/Header.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 function Header() {
   return (
     <>
-    <header>
+    <header className="flex items-center justify-between p-1 m-1 bg-red-400 text-white">
         <h2>Ecommerce</h2>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+        <ul className="flex items-center justify-between p-1 m-1">
+            <li className="p-2"><Link to="/">Home</Link></li>
+            <li className="p-2"><Link to="/about">About</Link></li>
+            <li className="p-2"><Link to="/blog">Blog</Link></li>
+            <li className="p-2"><Link to="/cart">Cart</Link></li>
+            <li className="p-2"><Link to="/contact">Contact</Link></li>
         </ul>
     </header>
     </>
--- vite-project/src/component/Product.jsx
+++ vite-project/src/component/Product.jsx
@@ -3,23 +3,32 @@ import { Link } from "react-router-dom";
 function Product({ product }) {
   const [cart, setCart] = useState([]);
   function handleAddToCart(product) {
-    setCart([...cart,product])
+    setCart([...cart, product]);
   }
   console.log(cart);
 
+  function shortTitle(title) {
+    return title.length > 30 ? title.substring(0, 30) + "..." : title;
+  }
+
   return (
-    <div className="product">
-      <div className="product-photo">
-        <img
-          src={product.image}
-          onClick={() => handleAddToCart(product)}
-          alt="productImage"
-        />
-      </div>
-      <div className="content">
-        <h3>{product.title}</h3>
-        <p>{product.price}</p>
-        <Link href="">Add To Cart</Link>
+    <div className="parent">
+      <div className="product">
+        <div className="product-photo h-60 w-50">
+          <img
+            src={product.image}
+            onClick={() => handleAddToCart(product)}
+            alt="productImage"
+          />
+        </div>
+        <div className="content text-center">
+          <h3>{shortTitle(product.title)}</h3>
+          <p>{product.price}</p>
+
+          <Link className="button bg-red-400 text-white p-1.5" to="">
+            Add To Cart
+          </Link>
+        </div>
       </div>
     </div>
   );
